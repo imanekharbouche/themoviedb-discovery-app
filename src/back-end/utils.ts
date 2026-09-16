b0ac23a371918377038ccf945e7282a3e3819fac
Imane KHARBOUCHE <imane.kharbouche@aol.com>
2026-09-16 15:07:50 +0000

    feat: ✨ improve /api/movies/popular endpoint to return supported Movie objects

--- src/back-end/utils.ts
+++ src/back-end/utils.ts
@@ -2,27 +2,25 @@ import type { TmdbMoviesRawResponse, Movie } from './schemas/MoviesTypes';
 
 /**
  * Transforms a TmdbMovie object into a supported Movie object by omitting the 'adult' and 'video' properties.
- * @param movie The raw TmdbMovie object.
+ * @param movieTmdb The raw TmdbMovie object.
  * @returns The supported Movie object.
  */
 
 
-export const toSupportedMovie = (movie: TmdbMoviesRawResponse['results'][number]): Movie => {
-
-
+export const toSupportedMovie = (movieTmdb: TmdbMoviesRawResponse['results'][number]): Movie => {
   return {
-    backdrop_path: movie.backdrop_path,
-    genre_ids: movie.genre_ids,
-    id: movie.id,
-    original_language: movie.original_language,
-    original_title: movie.original_title,
-    overview: movie.overview,
-    popularity: movie.popularity,
-    poster_path: movie.poster_path,
-    release_date: movie.release_date,
-    title: movie.title,
-    vote_average: movie.vote_average,
-    vote_count: movie.vote_count
+    backdrop_path: movieTmdb.backdrop_path,
+    genre_ids: movieTmdb.genre_ids,
+    id: movieTmdb.id,
+    original_language: movieTmdb.original_language,
+    original_title: movieTmdb.original_title,
+    overview: movieTmdb.overview,
+    popularity: movieTmdb.popularity,
+    poster_path: movieTmdb.poster_path,
+    release_date: movieTmdb.release_date,
+    title: movieTmdb.title,
+    vote_average: movieTmdb.vote_average,
+    vote_count: movieTmdb.vote_count
   };
 };
 
