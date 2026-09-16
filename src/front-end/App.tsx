5ce795c37f83a6c0b9c90ea602543fc3427371c5
Imane KHARBOUCHE <imane.kharbouche@aol.com>
2026-09-16 15:53:58 +0000

    feat: ✨ add MovieItem component for displaying individual movie details

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import type { Movie } from "../back-end/schemas/MoviesTypes"
+import MovieItem from "./components/MovieItem"
 
 export default function App() {
     useEffect(() => {
@@ -36,6 +37,13 @@ export default function App() {
               <p>Release Date: {movie.release_date}</p>
               <p>Rating: {movie.vote_average}</p>
             </li>
+          ))}
+        </ul>
+      ) : null}
+      {movies ? (
+        <ul>
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} movie={movie} />
           ))}
         </ul>
       ) : null}
